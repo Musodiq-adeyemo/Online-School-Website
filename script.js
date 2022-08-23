ed3516c893b95cd3699ae92c2af4afe849154ddf
ADEYEMO MUSODIQ OLALEKAN <adeyemomusodiq@gmail.com>
2022-08-23 23:16:33 +0100

    updated

---
+++ script.js
@@ -0,0 +1,273 @@
+// view button section
+const view1 =document.getElementById("view1");
+const view2 =document.getElementById("view2");
+const view3 =document.getElementById("view3");
+const view4 =document.getElementById("view4");
+const view5 =document.getElementById("view5");
+const view6 =document.getElementById("view6");
+const view7 =document.getElementById("view7");
+const view8 =document.getElementById("view8");
+const view9 =document.getElementById("view9");
+const view10 =document.getElementById("view10");
+const view11 =document.getElementById("view11");
+const view12 =document.getElementById("view12");
+
+//profile display section
+const profileDisplay1 =document.getElementById("PD1");
+const profileDisplay2 =document.getElementById("PD2");
+const profileDisplay3 =document.getElementById("PD3");
+const profileDisplay4 =document.getElementById("PD4");
+const profileDisplay5 =document.getElementById("PD5");
+const profileDisplay6 =document.getElementById("PD6");
+const profileDisplay7 =document.getElementById("PD7");
+const profileDisplay8 =document.getElementById("PD8");
+const profileDisplay9 =document.getElementById("PD9");
+const profileDisplay10 =document.getElementById("PD10");
+const profileDisplay11 =document.getElementById("PD11");
+const profileDisplay12 =document.getElementById("PD12");
+
+//view details section
+const views1 =document.getElementById("views1");
+const views2 =document.getElementById("views2");
+const views3 =document.getElementById("views3");
+const views4 =document.getElementById("views4");
+const views5 =document.getElementById("views5");
+const views6 =document.getElementById("views6");
+const views7 =document.getElementById("views7");
+const views8 =document.getElementById("views8");
+const views9 =document.getElementById("views9");
+const views10 =document.getElementById("views10");
+const views11 =document.getElementById("views11");
+const views12 =document.getElementById("views12");
+
+// profile details section
+const span1 =document.getElementById("span1");
+const span2 =document.getElementById("span2");
+const span3 =document.getElementById("span3");
+const span4 =document.getElementById("span4");
+const span5 =document.getElementById("span5");
+const span6 =document.getElementById("span6");
+const span7 =document.getElementById("span7");
+const span8 =document.getElementById("span8");
+const span9 =document.getElementById("span9");
+const span10 =document.getElementById("span10");
+const span11 =document.getElementById("span11");
+const span12 =document.getElementById("span12");
+
+// view section
+view1.addEventListener('click',() => {
+    if(views1.style.display ==="none") {
+        views1.style.display ="block";
+        console.log("yes")
+    } else {
+        views1.style.display="none"; 
+    }
+});
+
+view2.addEventListener('click',() => {
+    if(views2.style.display ==="none") {
+        views2.style.display ="block";
+        console.log("yes")
+    } else {
+        views2.style.display="none"; 
+    }
+});
+
+view3.addEventListener('click',() => {
+    if(views3.style.display ==="none") {
+        views3.style.display ="block";
+        console.log("yes")
+    } else {
+        views3.style.display="none"; 
+    }
+});
+
+view4.addEventListener('click',() => {
+    if(views4.style.display ==="none") {
+        views4.style.display ="block";
+        console.log("yes")
+    } else {
+        views4.style.display="none"; 
+    }
+});
+
+view5.addEventListener('click',() => {
+    if(views5.style.display ==="none") {
+        views5.style.display ="block";
+        console.log("yes")
+    } else {
+        views5.style.display="none"; 
+    }
+});
+
+view6.addEventListener('click',() => {
+    if(views6.style.display ==="none") {
+        views6.style.display ="block";
+        console.log("yes")
+    } else {
+        views6.style.display="none"; 
+    }
+});
+
+view7.addEventListener('click',() => {
+    if(views7.style.display ==="none") {
+        views7.style.display ="block";
+        console.log("yes")
+    } else {
+        views7.style.display="none"; 
+    }
+});
+
+view8.addEventListener('click',() => {
+    if(views8.style.display ==="none") {
+        views8.style.display ="block";
+        console.log("yes")
+    } else {
+        views8.style.display="none"; 
+    }
+});
+
+view9.addEventListener('click',() => {
+    if(views9.style.display ==="none") {
+        views9.style.display ="block";
+        console.log("yes")
+    } else {
+        views9.style.display="none"; 
+    }
+});
+
+view10.addEventListener('click',() => {
+    if(views10.style.display ==="none") {
+        views10.style.display ="block";
+        console.log("yes")
+    } else {
+        views10.style.display="none"; 
+    }
+});
+
+view11.addEventListener('click',() => {
+    if(views11.style.display ==="none") {
+        views11.style.display ="block";
+        console.log("yes")
+    } else {
+        views11.style.display="none"; 
+    }
+});
+
+view12.addEventListener('click',() => {
+    if(views12.style.display ==="none") {
+        views12.style.display ="block";
+        console.log("yes")
+    } else {
+        views12.style.display="none"; 
+    }
+});
+
+
+// profile section
+profileDisplay1.addEventListener('click',() => {
+    if(span1.style.display ==="none") {
+        span1.style.display ="block";
+        console.log("yes")
+    } else {
+        span1.style.display="none"; 
+    }
+});
+
+profileDisplay2.addEventListener('click',() => {
+    if(span2.style.display ==="none") {
+        span2.style.display ="block";
+        console.log("yes")
+    } else {
+        span2.style.display="none"; 
+    }
+});
+
+profileDisplay3.addEventListener('click',() => {
+    if(span3.style.display ==="none") {
+        span3.style.display ="block";
+        console.log("yes")
+    } else {
+        span3.style.display="none"; 
+    }
+});
+
+profileDisplay4.addEventListener('click',() => {
+    if(span4.style.display ==="none") {
+        span4.style.display ="block";
+        console.log("yes")
+    } else {
+        span4.style.display="none"; 
+    }
+});
+
+profileDisplay5.addEventListener('click',() => {
+    if(span5.style.display ==="none") {
+        span5.style.display ="block";
+        console.log("yes")
+    } else {
+        span5.style.display="none"; 
+    }
+});
+
+profileDisplay6.addEventListener('click',() => {
+    if(span6.style.display ==="none") {
+        span6.style.display ="block";
+        console.log("yes")
+    } else {
+        span6.style.display="none"; 
+    }
+});
+
+profileDisplay7.addEventListener('click',() => {
+    if(span7.style.display ==="none") {
+        span7.style.display ="block";
+        console.log("yes")
+    } else {
+        span7.style.display="none"; 
+    }
+});
+
+profileDisplay8.addEventListener('click',() => {
+    if(span8.style.display ==="none") {
+        span8.style.display ="block";
+        console.log("yes")
+    } else {
+        span8.style.display="none"; 
+    }
+});
+
+profileDisplay9.addEventListener('click',() => {
+    if(span9.style.display ==="none") {
+        span9.style.display ="block";
+        console.log("yes")
+    } else {
+        span9.style.display="none"; 
+    }
+});
+
+profileDisplay10.addEventListener('click',() => {
+    if(span10.style.display ==="none") {
+        span10.style.display ="block";
+        console.log("yes")
+    } else {
+        span10.style.display="none"; 
+    }
+});
+
+profileDisplay11.addEventListener('click',() => {
+    if(span11.style.display ==="none") {
+        span11.style.display ="block";
+        console.log("yes")
+    } else {
+        span11.style.display="none"; 
+    }
+});
+profileDisplay12.addEventListener('click',() => {
+    if(span12.style.display ==="none") {
+        span12.style.display ="block";
+        console.log("yes")
+    } else {
+        span12.style.display="none"; 
+    }
+});
